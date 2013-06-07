476a53fc03ca06f408f94940cb1a2048939ce628
Cubeology <jnj0414@gmail.com>
2013-06-07 14:48:06 -0400

    Test form of the mod

---
+++ nullcreeper_common/nullcreeper/lib/Reference.java
@@ -0,0 +1,8 @@
+package nullcreeper.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "NC";
+    public static final String MOD_NAME = "Null-Creeper";
+    public static final String VERSION = "0.0.1";
+}
